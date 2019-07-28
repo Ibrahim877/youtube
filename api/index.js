f82f3bafc79c77a37fcc3d29e914287a3c81269d
Ibrahim Memmedov <imamedoff1997@gmail>
2019-07-29 00:13:33 +0400

    make it better

--- api/index.js
+++ api/index.js
@@ -3,6 +3,17 @@ const app = express()
 
 
 app.post("/get_video", (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://nuxt-youtube.herokuapp.com/');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
   var video_url;
   var youtubedl = require('youtube-dl');
   var url = req.body.url;
